f98eb12a66ce846dbb5b22c276d891b66f9134be
Your Name <you@example.com>
2022-11-28 13:05:47 +0100

    path fixed

--- src/components/PageFrance.tsx
+++ src/components/PageFrance.tsx
@@ -1,6 +1,7 @@
 import { NavLink, Routes, Route } from "react-router-dom";
 import { PageParis } from "./PageParis";
 import { PageBordeaux } from "./PageBordeaux";
+import { Page404 } from "./Page404";
 export const PageFrance = () => {
   return (
     <>
@@ -12,6 +13,7 @@ export const PageFrance = () => {
       <Routes>
         <Route path="paris" element={<PageParis />} />
         <Route path="bordeaux" element={<PageBordeaux />} />
+        <Route path="*" element={<Page404 />} />
       </Routes>
     </>
   );
